4734c68fa0b87a00c8580955a03f9ba086d493c2
narayanrahulv <narayanrahulv@gmail.com>
2014-02-21 08:39:40 -0500

    Assignment 5 for database systems class
    Got a bit stumped by Q7. Tried to use the Min operator to find least
    number of products but wasn't able to figure out how to use it right.

---
+++ Lab5.sql
@@ -0,0 +1,67 @@
+﻿--Rahul Narayan: Lab 5 SQL Queries - The Three-quel
+--Database Systems: Spring 2014
+
+--1. The cities of agents booking an order for customer "Basics". This time use joins;no subqueries.
+SELECT	a.city
+FROM agents a
+	INNER JOIN orders o on
+		a.aid = o.aid	
+	INNER JOIN customers c on
+		o.cid = c.cid
+WHERE 
+	lower(c.name) = 'basics'
+	
+--2. The pids of products ordered through any agent who makes at least one order for a customer in Kyoto. Use joins this time;no subqueries.
+SELECT distinct p.pid
+FROM products p	
+	INNER JOIN orders o on
+		p.pid = o.pid
+	INNER JOIN customers c on
+		o.cid = c.cid
+WHERE 
+	lower(c.city) = 'kyoto'
+
+--3. The names of customers who have never placed an order. Use a subquery
+SELECT c.cid
+FROM customers c	
+where c.cid NOT IN
+	(SELECT DISTINCT cid
+	 FROM orders
+	)
+
+--4. The names of customers who have never placed an order. Use an outer join	
+SELECT c.name
+FROM customers c
+LEFT JOIN orders o on
+	c.cid = o.cid
+WHERE
+	o.cid is NULL	
+
+--5. The names of customers who had placed at least one order through an agent in their city along with those agents' names
+SELECT c.name,a.name,a.city
+FROM agents a
+	INNER JOIN orders o on
+		a.aid = o.aid	
+	INNER JOIN customers c on
+		o.cid = c.cid
+WHERE 
+	a.city = c.city	
+
+--6. The names of customers and agents in the same city, along with the name of the city, regardless of whether or not the customer has
+--ever placed an order with that agent	
+SELECT c.name,a.name,c.city
+FROM customers c
+LEFT JOIN orders o on
+	c.cid = o.cid
+LEFT JOIN agents a on
+	o.aid = a.aid
+WHERE
+	c.city = a.city
+
+--7. The name and city of customers who live in the city where the least number of products are made	
+select * from products p
+INNER JOIN orders o on
+	p.pid = o.pid
+INNER JOIN customers c on
+	o.cid = c.cid
+--where quantity = min(quantity)
