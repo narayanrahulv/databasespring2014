e378b898e90b8c70f26b95a99e1960f499794023
narayanrahulv <narayanrahulv@gmail.com>
2014-04-04 08:49:58 -0400

    lab 8
    included create and inserts sql file with commit in case it is needed

---
+++ lab8 inserts.sql
@@ -0,0 +1,101 @@
+﻿--insert into people table
+insert into people (PID, FName, LName, MI, Gender, Birth)
+Values (1,'Johnny', 'Depp', '', 'm', '1970-09-08');	
+insert into people (PID, FName, LName, MI, Gender, Birth)
+Values (2,'Sean', 'Connery', '', 'm', '1940-06-21');
+insert into people (PID, FName, LName, MI, Gender, Birth)
+Values (3,'Amy', 'Poehler', '', 'f', '1972-04-21');
+insert into people (PID, FName, LName, MI, Gender, Birth)
+Values (4,'Martin', 'Scorcese', '', 'm', '1950-03-09');
+insert into people (PID, FName, LName, MI, Gender, Birth)
+Values (5,'Tina', 'Fey', '', 'f', '1971-03-30');
+
+--insert into address table
+insert into address(PID, Addr1, Addr2, City, State, Zip, Country)
+values (1,'400 rue st clair','40','Marseille','','00082','France');
+insert into address(PID, Addr1, Addr2, City, State, Zip, Country)
+values (1,'40 beverly hils','','Los Angeles','CA','90210','USA');
+
+insert into address(PID, Addr1, Addr2, City, State, Zip, Country)
+values (2,'10 downing st','flat 8','London','','H21-89J','UK');
+insert into address(PID, Addr1, Addr2, City, State, Zip, Country)
+values (2,'100 country lane','','Smithtown','NY','10090','USA');
+
+insert into address(PID, Addr1, Addr2, City, State, Zip, Country)
+values (3,'30 Chiswick rd','Apt 8E','Boston','MA','09876','USA');
+
+insert into address(PID, Addr1, Addr2, City, State, Zip, Country)
+values (4,'900 San Quentin rd','','Pasadena','CA','90876','USA');
+
+insert into address(PID, Addr1, Addr2, City, State, Zip, Country)
+values (5,'200 park ave','6M','New York','NY','10221','USA');
+
+--insert into actor table
+insert into actor(PID, Hair, Eyes, HeightInches, WeightLbs, SAGDate)
+values(1, 'BL', 'BR', 36, 160, '2000-03-04');
+insert into actor(PID, Hair, Eyes, HeightInches, WeightLbs, SAGDate)
+values(2, 'BL', 'BR', 42, 180, '1984-03-04');
+insert into actor(PID, Hair, Eyes, HeightInches, WeightLbs, SAGDate)
+values(3, 'BL', 'BL', 30, 120, '2002-03-04');
+insert into actor(PID, Hair, Eyes, HeightInches, WeightLbs, SAGDate)
+values(5, 'BR', 'BR', 30, 120, '1999-03-04');
+
+--insert into director table
+insert into director(PID, SCode, DirectorSAGDate)
+values(2, 'UKFS', '1984-02-02');
+insert into director(PID, SCode, DirectorSAGDate)
+values(4, 'Bkly', '1998-12-02');
+insert into director(PID, SCode, DirectorSAGDate)
+values(5, 'NYU', '1998-12-02');
+
+--insert into hairinfo table
+insert into hairInfo(ColorCode, ColorDescr)
+values('BL','black');
+insert into hairInfo(ColorCode, ColorDescr)
+values('BO','blonde');
+insert into hairInfo(ColorCode, ColorDescr)
+values('BR','brown');
+
+--insert into eyeinfo table
+insert into eyeInfo(ColorCode, ColorDescr)
+values('BL','blue');
+insert into eyeInfo(ColorCode, ColorDescr)
+values('BR','brown');
+
+--insert into movies table
+insert into movies(Name, RelYear, BoxOfficeUSD, BoxOfficeIntl, CurCode, DVDBluRayUSD)
+values('Pirates of the Caribbean','2003','40 bil','20 mil','GBP','50 bil');
+insert into movies(Name, RelYear, BoxOfficeUSD, BoxOfficeIntl, CurCode, DVDBluRayUSD)
+values('The Departed','2007','20 mil','10 mil','INR','60 mil');
+insert into movies(Name, RelYear, BoxOfficeUSD, BoxOfficeIntl, CurCode, DVDBluRayUSD)
+values('Never Say Never Again','1983','50 bil','90 mil','AUD','60 mil');
+
+--insert into actorwork table
+insert into actorWork(PID, Name, RelYear)
+values(2,'Never Say Never Again','1983');
+insert into actorWork(PID, Name, RelYear)
+values(1,'Pirates of the Caribbean','2003');
+
+--insert into directorwork table
+insert into directorWork(PID, Name, RelYear)
+values(4,'The Departed','2007');
+insert into directorWork(PID, Name, RelYear)
+values(4,'Never Say Never Again','1983');
+
+--insert into currency table
+insert into currency(CurCode, CurDescr)
+values('GBP','Great Britain Pounds');
+insert into currency(CurCode, CurDescr)
+values('INR','Indian Rupees');
+insert into currency(CurCode, CurDescr)
+values('AUD','Australian Dollar');
+insert into currency(CurCode, CurDescr)
+values('USD','USA Dollar');
+
+--insert into film school table
+insert into filmschool(SchoolCode,SchoolDescr)
+values('UKFS','UK Film School');
+insert into filmschool(SchoolCode,SchoolDescr)
+values('Bkly','Berkely');
+insert into filmschool(SchoolCode,SchoolDescr)
+values('NYU','NYU Film School');
---
+++ lab8.sql
@@ -0,0 +1,85 @@
+﻿--people table
+CREATE TABLE people (
+	PID	integer PRIMARY KEY,
+	FName	varchar(100) NOT NULL,
+	LName	varchar(100) NOT NULL,
+	MI	varchar(1),
+	Gender	varchar(1),
+	Birth	date
+);
+
+--address table
+CREATE TABLE address (
+	PID	integer NOT NULL,
+	Addr1	varchar(100) NOT NULL,
+	Addr2	varchar(100),
+	City	varchar(50) NOT NULL,
+	State	varchar(2) NOT NULL,
+	Zip	varchar(10) NOT NULL,	
+	Country	varchar(50)
+);
+
+--actor table
+CREATE TABLE actor (
+	PID		integer NOT NULL,
+	Hair		varchar(2),
+	Eyes		varchar(2),
+	HeightInches	integer,
+	WeightLbs	integer,
+	SAGDate		date
+);
+
+--director table
+CREATE TABLE director (
+	PID		integer NOT NULL,
+	SCode		varchar(10),
+	DirectorSAGDate	date
+);
+
+--hairInfo table
+CREATE TABLE hairInfo (
+	ColorCode	varchar(2),
+	ColorDescr	varchar(100)
+);
+
+--eyeInfo table
+CREATE TABLE eyeInfo (
+	ColorCode	varchar(2),
+	ColorDescr	varchar(100)
+);
+
+--movie table
+CREATE TABLE movies (
+	Name		varchar(100) NOT NULL,
+	RelYear		varchar(4) NOT NULL,
+	BoxOfficeUSD	varchar(6),
+	BoxOfficeIntl	varchar(6),
+	CurCode		varchar(4),
+	DVDBluRayUSD	varchar(6)
+);
+
+--actor work table
+CREATE TABLE actorWork (
+	PID	integer,
+	Name	varchar(100),
+	RelYear	varchar(4)
+);
+
+--director work table
+CREATE TABLE directorWork (
+	PID	integer,
+	Name	varchar(100),
+	RelYear	varchar(4)
+);
+
+--currency table
+CREATE TABLE currency (
+	CurCode		varchar(4),
+	CurDescr	varchar(20)
+);
+
+--film school table
+CREATE TABLE filmschool (
+	SchoolCode	varchar(4),
+	SchoolDescr	varchar(100)
+);
