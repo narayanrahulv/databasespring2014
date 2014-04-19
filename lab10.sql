2bc73225a13b04000e2bf23adcaa06a72b95296f
narayanrahulv <narayanrahulv@gmail.com>
2014-04-19 18:43:51 -0400

    Lab 10 files: Rahul Narayan
    Includes the create script from the website to create courses/prereqs
    table (and sample function) as well as script to create PreReqFor and
    IsPreReqFor procedures.

---
+++ lab10.sql
@@ -0,0 +1,54 @@
+﻿--Rahul Narayan: Lab 10
+--Procedure 1: function PreReqsFor: Returns the immediate prerequisites for the passed in course number
+create or replace function PreReqsFor(int, REFCURSOR) returns refcursor as 
+$$
+declare
+	course_num 	int		:= $1;
+	prereqs 	REFCURSOR	:= $2;
+begin
+	open prereqs for
+		select max(prereqnum) 
+		from Prerequisites
+		where coursenum = course_num;
+	return prereqs;
+end;
+$$
+language plpgsql;
+
+select 	PreReqsFor(499,'immediateprereqs');
+fetch all from immediateprereqs;
+select 	PreReqsFor(308,'immediateprereqs');
+fetch all from immediateprereqs;
+select 	PreReqsFor(221,'immediateprereqs');
+fetch all from immediateprereqs;
+select 	PreReqsFor(220,'immediateprereqs');
+fetch all from immediateprereqs;
+select 	PreReqsFor(120,'immediateprereqs');
+fetch all from immediateprereqs;	
+
+--Procedure 2: function IsPreReqFor: Returns the courses for which the passed in course number is an immediate prerequisite
+create or replace function IsPreReqFor(int, REFCURSOR) returns refcursor as 
+$$
+declare
+	course_num 	int		:= $1;
+	isprereqfor 	REFCURSOR	:= $2;
+begin
+	open isprereqfor for
+		select coursenum 
+		from Prerequisites
+		where prereqnum = course_num;
+	return isprereqfor;
+end;
+$$
+language plpgsql;
+
+select 	IsPreReqFor(499,'subcourses');
+fetch all from subcourses;
+select 	IsPreReqFor(308,'subcourses');
+fetch all from subcourses;
+select 	IsPreReqFor(221,'subcourses');
+fetch all from subcourses;
+select 	IsPreReqFor(220,'subcourses');
+fetch all from subcourses;
+select 	IsPreReqFor(120,'subcourses');
+fetch all from subcourses;	
