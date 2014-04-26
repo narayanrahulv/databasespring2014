fdaf64d26b5a73ed97430591bae703d8b26be4df
narayanrahulv <narayanrahulv@gmail.com>
2014-04-25 22:19:01 -0400

    associated files for design project
    screenshot png files and create reports script

---
+++ design project/create reports.sql
@@ -0,0 +1,24 @@
+﻿--GetInjuryListing
+select getinjurydetails('injurylisting');
+fetch all from injurylisting;
+
+--GetWorkPermitListing
+	--need work permit
+	select getworkpermitdetails(true,'workpermit');
+	fetch all from workpermit;
+
+	--don't need work permit
+	select getworkpermitdetails(false,'workpermit');
+	fetch all from workpermit;
+
+--GetSponsorDetails
+select getsponsordetails('2011','sponsordetails');
+fetch all from sponsordetails;
+
+--TournamentParticipation
+select gettournamentparticipation('2014','tournamentdetails');
+fetch all from tournamentdetails;
+
+--EmployeeDetails
+select getemployeesbytype('managers','employeetypes');
+fetch all from employeetypes;
